8b3ee435bbf8043aefd8edbdc4a233db40763f31
MulubahzumuKSipor <156727431+MulubahzumuKSipor@users.noreply.github.com>
2024-10-10 00:50:05 +0000

    Add files via upload

---
+++ chamber/scripts/join.js
@@ -0,0 +1,80 @@
+import {members} from "../scripts/join.mjs"
+
+console.log(members)
+
+const modals = document.querySelector("#modal");
+const closet = document.querySelector("#close-button");
+
+const infoContainer = document.querySelector("#promoCards")
+
+function display(aList) {
+    infoContainer.innerHTML = "";
+    aList.forEach(element => {
+        const container = document.createElement("section");
+        container.classList.add('section')
+
+        const titleTag = document.createElement("p");
+        const buttonTag = document.createElement("button");
+        
+
+        const title = `${element.name}`;
+        const buttons = "Learn more";
+        
+
+        titleTag.innerHTML = title;
+        buttonTag.innerHTML = buttons;
+
+        buttonTag.addEventListener('click', () =>{
+            displayInfo(element)
+        })
+        
+        if (element.name === "Bronze Membership"){
+        container.classList.add("bronze");
+        }
+        else if (element.name === "Silver Membership"){
+            container.classList.add("silver")
+        }
+        else if(element.name === "Gold Membership"){
+            container.classList.add("premium")}
+        
+        else {container.classList.add("normal")}
+
+        container.appendChild(titleTag);
+        container.appendChild(buttonTag)
+        
+
+        infoContainer.append(container);
+        
+    });
+}
+
+
+
+display(members);
+const info1 = document.querySelector("#info1");
+const info2 = document.querySelector("#info2");
+const info3 = document.querySelector("#info3");
+const info4 = document.querySelector("#info4");
+const info5 = document.querySelector("#info5");
+const myTitle = document.querySelector("#title h2");
+
+
+
+closet.addEventListener('click', () => {modals.close();})
+
+
+
+function displayInfo(elements){
+
+        modals.showModal();
+        myTitle.innerHTML = `${elements.name}`;
+        info1.innerHTML = `${elements.benefits[0]}`
+        info2.innerHTML = `${elements.benefits[1]}`
+        info3.innerHTML = `${elements.benefits[2]}`
+        info4.innerHTML = `${elements.benefits[3]}`
+        info5.innerHTML = `${elements.benefits[4]}`
+    
+};
+
+
+
---
+++ chamber/scripts/join.mjs
@@ -0,0 +1,40 @@
+export const members = [
+    {name:"Normal Participant", 
+        benefits:[ "Access to Social media, Website information, Newsletter and Announcement",
+                    "Allow to send feedback and recommendation",
+                    "A Chamber of Commerce Account",
+                    "Early notifications to upcoming products",
+                    "Access to Pre-order on any product available"
+            
+        ], 
+        price:"$0.00/months"},
+    
+        {name:"Bronze Membership", 
+        benefits:[  "Normal Participant benefits",
+                    "25% Discount on all products",
+                    "Access to all regular events",
+                    "Business gets displayed on our Directory page",
+                    "Possibility of connecting with high ends customer",
+        ],
+        price:"$30.00/months"
+        
+        },
+        {name:"Silver Membership", 
+        benefits:[  "Bronze Members benefits",
+                    "35% Discount on all products",
+                    "Access to all VIP events",
+                    "Business gets displayed on our Directory page",
+                    "Connections with high ends customer",
+            
+                ], 
+        price:"$50.00/months"}, 
+        {name:"Gold Membership", 
+        benefits:[ "50% Discount on all Products and services",
+                    "Free access to all Events",
+                    "Administrator access on website", 
+                    "Can impact decision making on which silver member gets to meet an high end customer",
+                    "Personal contact of every customer"
+            
+        ],
+        price:"$75.00/months"}
+]
