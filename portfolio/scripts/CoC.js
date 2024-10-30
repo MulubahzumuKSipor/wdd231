468f57bdbf59c95b80c566cd90d151ae8e9bb1b8
MulubahzumuKSipor <156727431+MulubahzumuKSipor@users.noreply.github.com>
2024-10-30 02:10:57 +0000

    Delete portfolio directory

--- portfolio/scripts/CoC.js
+++
@@ -1,4 +0,0 @@
-// Time
-const lastmod = document.querySelector("#last_modified");
-lastmod.innerHTML = new Date().toLocaleDateString("en-US", Option);
-document.getElementById("year").innerHTML = new Date().getFullYear();
